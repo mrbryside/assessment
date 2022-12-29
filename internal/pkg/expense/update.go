d5121e1e0a42d0fc68aa065e50599b966cdffb6c
mrbryside <sirawat.i@ku.th>
2022-12-29 15:35:50 +0700

    add get real param id in update expense

--- internal/pkg/expense/update.go
+++ internal/pkg/expense/update.go
@@ -3,11 +3,17 @@ package expense
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/mrbryside/assessment/internal/pkg/util"
+	"strconv"
 )
 
 func (e *expense) UpdateExpenseHandler(c echo.Context) error {
+	idParam := c.Param("id")
+	id, _ := strconv.Atoi(idParam)
 	model := newModelExpense()
+
 	_ = c.Bind(&model)
-	model.ID = 1
+
+	model.ID = id
+
 	return util.JsonHandler().Success(c, model)
 }
